f6707c1a005fd4c9a6ee2ce9fa71be6755ea4b42
TonyTonyTonyTony1 <antonio.ccardoso99@gmail.com>
2025-10-22 21:13:04 -0300

    Add files via upload

---
+++ aprendendo-logica-programacao/operadores-aritimeticos.js
@@ -0,0 +1,5 @@
+console.log(2 + 3)/* adiçao */
+console.log(2 - 3)/* subtraçao */
+conseole.log(2 * 3)/* multiplicacao */
+console.log(2 / 3)/* divisao */
+console.log(2 % 3)/* o resto da divisao = 1 */
---
+++ aprendendo-logica-programacao/tipos-de-dados.js
@@ -0,0 +1,11 @@
+let nome = "antonio" /*string */
+let idade ="24" /* number */
+let temBarba = true /* boolean */
+let pet /* undefined */
+
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof temBarba)
+console.log(typeof pet)
+
+console.log()
